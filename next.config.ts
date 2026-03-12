5d38777ee3265ce9f6829a1eb9a8ce4b2f5f55bb
Adriel Izoton de Faci <adriel.idf@gmail.com>
2026-03-12 17:48:46 -0300

    refactor: Rename application from 'p-gatcha-app' to 'movies-pack-opener-app' in package name and Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   output: 'export',
   // Adds the repository name as a prefix to all paths
   // Required for GitHub Pages when hosting on a subpath rather than a custom domain
-  basePath: isProd ? '/p-gatcha-app' : '',
-  assetPrefix: isProd ? '/p-gatcha-app/' : '',
+  basePath: isProd ? '/movies-pack-opener-app' : '',
+  assetPrefix: isProd ? '/movies-pack-opener-app/' : '',
 };
 
 export default nextConfig;
